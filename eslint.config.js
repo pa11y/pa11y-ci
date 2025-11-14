2743e820530f382d2703c95280f52eb2fae762bf
Danyal Aytekin <danyal@fubase.com>
2025-11-14 20:45:11 +0000

    Move ignore rule into config

--- eslint.config.js
+++ eslint.config.js
@@ -5,5 +5,10 @@ const {defineConfig} = require('eslint/config');
 const configPa11y = require('eslint-config-pa11y');
 
 module.exports = defineConfig([
-	configPa11y
+	configPa11y,
+	{
+		ignores: [
+			'test/integration/mock/config/syntax-errors-js.js'
+		]
+	}
 ]);
